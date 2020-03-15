e8b43a31e5a7e84fe030d1976572a163a885fc94
Marwen Magri <marwen.magri@gomycode.co>
2020-03-15 22:43:33 +0100

    connect movieContainer and bring the state to display the movies up

--- src/component/CardContainer.js
+++ src/component/CardContainer.js
@@ -5,37 +5,57 @@ import NameFilter from "./NameFilter.js";
 import Rate from "./Rating";
 import Modal from "./Modal";
 import { message } from "antd";
+
+
+
 class CardContainer extends Component {
+
   state = {
+
     movies:this.props.movies,
 
     nameFilter: "",
+
     minRatingFilter: 0,
+
     deletedMovie: ""
+
   };
+
+  //
   addNewMovie = (img, name, star) => {
     this.setState({
       movies: this.state.movies.concat({ img, star, name })
     });
   };
+
+  //
   handleSearch = input => {
     this.setState({
       nameFilter: input
     });
   };
+
+  //
   error = name => {
     message.error(`You did deleted this movie : ${name}`);
   };
+
+  //
   handlDelete = movie => {
     this.setState({
       movies: this.state.movies.filter(el => el.name !== movie)
     });
   };
+
+  //
   handleChange = newRating => {
     this.setState({
       minRatingFilter: newRating
     });
   };
+
+
 
   render() {
     const filtered = this.state.movies.filter(
@@ -43,29 +63,41 @@ class CardContainer extends Component {
         el.star >= this.state.minRatingFilter &&
         el.name.toLowerCase().includes(this.state.nameFilter.toLowerCase())
     );
+
     return (
+
       <div className="cardContainer">
+
         <div className="header">
+
           <NameFilter handleSearch={this.handleSearch} b={this.searchMovies} />
+          
           <Modal add={this.addNewMovie} />
 
           <Rate
             minRate={this.state.minRatingFilter}
             handleRate={this.handleChange}
           />
+
         </div>
+
         <CardList
           movies={filtered}
           deleteMovie={this.handlDelete}
           error={this.error}
         />
+
       </div>
+
     );
   }
 }
+
+
 const mapStateToProps = state => ({
   movies: state.movieReducer.movies,
 
 });
+
 
 export default connect(mapStateToProps)(CardContainer);
